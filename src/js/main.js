e6b3fa345dad5fe5cf0f94c46be8b262d71b4b4d
Omri Mor <omri@Omris-MacBook-Pro.local>
2015-08-20 08:47:53 +0300

    fixed bug when unfocus from input value is reset. unified iOS & Android cards to one.

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,18 @@
 $(function(){
 	'use strict';
 
-	$('#android').addClass('selected');
-	$('.androidLogo').attr('src', 'images/botSelected.svg');
-	$('.detector').attr('data-name', 'android');
-
 	/**
 	 * Handle the convertor
 	 */
 
 	// Define Arrays for Android & iOS
-	var bucketsAndroid = [
+	var buckets = [
 	    { name:'LDPI', id:'ldpi', density:0.75 },
-	    { name:'Base / MDPI', id : 'base', density:1 },
+	    { name:'MDPI / Base', id : 'base', density:1 },
 	    { name:'HDPI', id:'hdpi', density:1.5 },
-	    { name:'Retina / XHDPI', id:'retina', density:2 },
+	    { name:'XHDPI / Retina', id:'retina', density:2 },
 	    { name:'XXHDPI', id:'xxhdpi', density:3 },
 	    { name:'XXXHDPI', id:'xxxhdpi', density:4 }
-	];
-
-	var bucketsiOS = [
-	    { name:'Base-1x', id:'1', density: 1 },
-	    { name:'Retina-2x', id: '2', density: 2 },
-	    { name:'Retina-3x', id:'3', density: 3 }
 	];
 
 	var converterCalculator = function(bucketsObj, bucketsContainer){
@@ -34,14 +24,15 @@ $(function(){
 										name:'width[]', value:'0', autocomplete : 'off', maxlength: 5}),
 					bucketWidthLabel = $('<label>').attr('for', 'bucketWidth_'+i).html('0');
 
-				bucketName.appendTo(bucket).html(bucketsObj[i].name + ' <span>' + bucketsObj[i].density + 'x</span>');
+				bucketName.appendTo(bucket).html(bucketsObj[i].name +
+					'  <span>' + 'x' + bucketsObj[i].density + '</span>');
 				bucketWidth.appendTo(bucket);
 				bucketWidthInput.appendTo(bucketWidth);
 				bucketWidthLabel.appendTo(bucketWidth);
 
 				// Disable all inputs but numbers
 				bucketWidthInput.on('keydown', function(e) {
-					if((e.keyCode !== 8)&& (e.keyCode !== 9) && !( e.keyCode >= 48 &&
+					if((e.keyCode !== 8) && (e.keyCode !== 9) && !( e.keyCode >= 48 &&
 						e.keyCode <= 57 ) && !( e.keyCode >= 96 && e.keyCode <= 105 )){
 							e.preventDefault();
 					}
@@ -54,15 +45,13 @@ $(function(){
 					if((e.keyCode === 8) || (e.keyCode === 9) || ( e.keyCode >= 48 && e.keyCode <= 57 ) ||
 		   			   (e.keyCode >= 96 && e.keyCode <= 105 )){
 							$('input[name=\'width[]\']').each(function(a){
-
 							// Perform calc to all but currently selected input fiels
 							if(i !== a){
 								$(this).val(Math.ceil(base * bucketsObj[a].density));
-
-								// Change to label according to input value
-								$('label[for=\'' + $(this).attr('id') + '\']')
-								.html(Math.ceil(base * bucketsObj[a].density));
 							}
+							// Change to label according to input value
+							$('label[for=\'' + $(this).attr('id') + '\']')
+							.html(Math.ceil(base * bucketsObj[a].density));
 						});
 					} else {
 						e.preventDefault();
@@ -82,43 +71,7 @@ $(function(){
 			});
 	};
 
-	/**
-	 * Handle toggeling the views
-	 */
-
-	var toggleView = function(e){
-		e.preventDefault();
-
-		if($(this).attr('id') === 'ios'){
-			$('.flipper').addClass('rotate');
-			$('#android').removeClass('selected');
-			$('#ios').addClass('selected');
-			$('.appleLogo').attr('src', 'images/appleSelected.svg');
-			$('.androidLogo').attr('src', 'images/bot.svg');
-			$('.detector').attr('data-name', 'ios');
-
-			$('.buckets-ios label').each(function(index, elm) {
-				$(this).text('0');
-			});
-		}
-		if($(this).attr('id') === 'android'){
-			$('.flipper').removeClass('rotate');
-			$('#ios').removeClass('selected');
-			$('#android').addClass('selected');
-			$('.appleLogo').attr('src', 'images/apple.svg');
-			$('.androidLogo').attr('src', 'images/botSelected.svg');
-			$('.detector').attr('data-name', 'android');
-
-			$('.buckets-android label').each(function(index, elm) {
-				$(this).text('0');
-			});
-		}
-	};
-
-	converterCalculator(bucketsAndroid, '.buckets-android');
-	converterCalculator(bucketsiOS, '.buckets-ios');
-
-	$('[data-link="flipBtn"]').on('click', toggleView);
+	converterCalculator(buckets, '.buckets');
 });
 
 
